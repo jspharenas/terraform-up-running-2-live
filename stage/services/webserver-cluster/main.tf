61d726a6034dee9d628260b97ac42d28d6e871cf
Joseph Arenas <hotsoup2.0@icloud.com>
2019-03-21 11:31:35 -0400

    Changed module source to GitHub.

--- stage/services/webserver-cluster/main.tf
+++ stage/services/webserver-cluster/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "webserver_cluster" {
-  source = "../../../modules/services/webserver-cluster"
+  source = "git::git@github.com:jspharenas/terraform-up-running-2-modules.git//services/webserver-cluster?ref=v0.0.1"
 
   cluster_name = "webservers-stage"
   db_remote_state_bucket = "f0084r-terraform-up-running-remote-state"
